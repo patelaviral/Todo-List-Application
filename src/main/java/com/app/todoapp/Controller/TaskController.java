2dc52b8480abff5b6ff8b23fbf463d67811cfcb4
Aviral <aviralpatel911@gmail.com>
2025-11-15 20:18:50 +0530

    Initial commit

---
+++ src/main/java/com/app/todoapp/Controller/TaskController.java
@@ -0,0 +1,45 @@
+package com.app.todoapp.Controller;
+
+import com.app.todoapp.Models.Task;
+import com.app.todoapp.Service.TaskService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+//@RequestMapping("/tasks")
+public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public String getTasks(Model model) {
+        List<Task> tasks = taskService.getAllTasks();
+        model.addAttribute("tasks", tasks);
+        return "tasks";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/toggle")
+    public String toggleTask(@PathVariable Long id) {
+        taskService.toggleTask(id);
+        return "redirect:/";
+    }
+
+    @PostMapping
+    public String getTasks(@RequestParam String title) {
+        taskService.createTask(title);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/app/todoapp/Service/TaskService.java
@@ -0,0 +1,38 @@
+package com.app.todoapp.Service;
+
+import com.app.todoapp.Models.Task;
+import com.app.todoapp.Repository.TaskRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+    private final TaskRepository taskRepository;
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public void createTask(String title) {
+        Task task = new Task();
+        task.setTitle(title);
+        task.setCompleted(false);
+        taskRepository.save(task);
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+
+    public void toggleTask(Long id) {
+        Task task = taskRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid Task id"));
+        task.setCompleted(!task.isCompleted());
+        taskRepository.save(task);
+
+    }
+}
